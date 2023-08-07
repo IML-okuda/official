e6889a19582feb785437f96c2896bc97c732a78e
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-08-07 16:48:34 +0900

    chore: update button example

---
+++ client/src/ui/button/button.tsx
@@ -0,0 +1,34 @@
+import {
+  ComponentPropsWithoutRef,
+  ForwardedRef,
+  ReactNode,
+  forwardRef,
+} from "react";
+
+import * as styles from "./button.css";
+
+type Props = ComponentPropsWithoutRef<"button"> & {
+  variant?: "primary" | "secondary";
+  children: ReactNode;
+};
+
+const _Button = forwardRef(
+  (
+    { variant = "primary", children, ...props }: Props,
+    ref: ForwardedRef<HTMLButtonElement>
+  ) => {
+    return (
+      <button
+        ref={ref}
+        {...props}
+        className={styles.buttonVariantStyle[variant]}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+_Button.displayName = "Button";
+
+export const Button = _Button;
