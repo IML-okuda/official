8bc96008286fe9e94d9125b1d28d7499e70e4583
AOKI Takashi <55625375+RyushiAok@users.noreply.github.com>
2023-12-14 01:00:03 +0900

    chore(deps): react, vanilla extract plugins

--- sakura/astro.config.mjs
+++ sakura/astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  vite:{
+    plugins:[vanillaExtractPlugin()],
+  },
   integrations: [react()], 
 });
